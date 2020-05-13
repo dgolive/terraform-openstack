8a08aafbe0a61f70d2c390e9f609e01f9d1785ab
DgoLive <42310282+dgolive@users.noreply.github.com>
2020-05-13 16:51:49 -0300

    provider.tf-v1
    v1

---
+++ provider.tf
@@ -0,0 +1,10 @@
+provider "openstack" {
+  auth_url              = "http://10.20.30.4:5000/v3"
+  project_domain_id     = "if89asf98s7f8as98s7s8g8gs978"
+  region                = "RegionOne"
+  tenant_name           = "tenant"
+  user_domain_name      = "domain"
+  user_name             = "useradmin"
+  password              = "xxxxxxxxx"
+  tenant_id             = "adadadadaasdasd"
+}
